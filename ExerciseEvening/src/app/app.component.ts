5d68eae14f96ee0d875351683dd81a8f4ec0d712
Vladyslav Okopnyy <harmfulcati@gmail.com>
2023-03-20 17:12:01 +0100

    Esercizio completato con Input-ngContent-Output

--- ExerciseEvening/src/app/app.component.ts
+++ ExerciseEvening/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChildComponent } from './components/child/child.component';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ExerciseEvening';
+
+  data :string = "";
+
+  Nomi = [
+    { id: 1, name: 'Carlo' },
+    { id: 2, name: 'Paola' },
+    { id: 3, name: 'Vittorio' },
+  ];
+
+  onNameSelected(Object: string) {
+    console.log('Selected Name:', Object);
+  }
+
+  // Creo un istanza figlio del tipo del figlio (ChildComponent) nel padre
+  @ViewChild(ChildComponent) child!: ChildComponent;
+
+  //Questa funzione viene richiamata da figlio e i dati si uniscono
+  CurrentDate(){
+    const today = new Date();
+    //metto la data in una stringa
+    this.data = today.toLocaleDateString('it-IT');
+    // passo la data alla funzione "ViewData"
+    this.child.ViewData(this.data);
+  }
+
+  constructor(){
+
+  }
 }
--- ExerciseEvening/src/app/components/child/child.component.ts
+++ ExerciseEvening/src/app/components/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./child.component.css']
 })
 export class ChildComponent {
+
+  @Input() names: any[] = [];
+
+  @Output()
+  selected = new EventEmitter();
+
+  onNameSelected(name: string) {
+    this.selected.emit(name);
+  }
+
+  buongiorno(){
+    console.log("Buongiorno Simona :)");
+  }
+
+  ViewData(nome : string){
+    console.log("La data di oggi: " + nome);
+  }
 
 }
